9155d1266b72ee8c8a7286645adec1a950aafcdc
rmaharan <ranjita.maharana12@gmail.com>
2025-10-03 14:42:03 +1000

    Update Warehouse.sql

--- 0001-schema_objects/Warehouse.sql
+++ 0001-schema_objects/Warehouse.sql
@@ -41,7 +41,7 @@ USE ROLE ACCOUNTADMIN;
 ALTER WAREHOUSE {{env}}_EDW_WHS
     SET RESOURCE_MONITOR = "{{env}}_EDW_REPORTING_WHS_MONITOR";
 
-CREATE TAG environment_tag;
+CREATE OR REPLACE TAG environment_tag;
 ALTER WAREHOUSE {{env}}_EDW_WHS SET TAG environment_tag = '{{env}}';
 
 
